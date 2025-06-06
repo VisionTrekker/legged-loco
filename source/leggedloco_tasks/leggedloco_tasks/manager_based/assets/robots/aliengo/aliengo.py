3bbf5cf3c989322a4b43258dbcc2500e5ed0e3d5
liuzhi <18082258131@163.com>
2025-06-06 14:28:27 +0800

    simplify the aliengo cfg

--- source/leggedloco_tasks/leggedloco_tasks/manager_based/assets/robots/aliengo/aliengo.py
+++ source/leggedloco_tasks/leggedloco_tasks/manager_based/assets/robots/aliengo/aliengo.py
@@ -88,75 +88,13 @@ UNITREE_ALIENGO_DCMOTOR_CFG = ArticulationCfg(
 """Configuration of Unitree AlienGo using DC-Motor actuator model."""
 
 
-UNITREE_ALIENGO_DELAYEDPD_CFG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(
-        # usd_path=f"http://omniverse-content-production.s3-us-west-2.amazonaws.com/Assets/Isaac/4.1/Isaac/Robots/Unitree/aliengo/aliengo.usd",
-        usd_path=f"{os.getenv('USER_PATH_TO_USD')}/robots/aliengo/aliengo.usd",
-        activate_contact_sensors=True,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            retain_accelerations=False,
-            linear_damping=0.0,
-            angular_damping=0.0,
-            max_linear_velocity=1000.0,
-            max_angular_velocity=1000.0,
-            max_depenetration_velocity=1.0,
-        ),
-        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
-        ),
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.55),
-        joint_pos={
-            ".*L_hip_joint": 0.1,
-            ".*R_hip_joint": -0.1,
-            "F[L,R]_thigh_joint": 0.8,
-            "R[L,R]_thigh_joint": 1.0,
-            ".*_calf_joint": -1.5,
-        },
-        joint_vel={".*": 0.0},
-    ),
-    soft_joint_pos_limit_factor=0.9,
-    actuators={
-        "base_legs": ALIENGO_ACTUATOR_DELAYEDPD_CFG,
-    },
+UNITREE_ALIENGO_DELAYEDPD_CFG = UNITREE_ALIENGO_DCMOTOR_CFG.replace(
+    actuators={"base_legs": ALIENGO_ACTUATOR_DELAYEDPD_CFG}
 )
 """Configuration of Unitree AlienGo using DelayedPD actuator model."""
 
 
-UNITREE_ALIENGO_IMPLICIT_CFG = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(
-        usd_path=f"{os.getenv('USER_PATH_TO_USD')}/robots/aliengo/aliengo.usd",
-        activate_contact_sensors=True,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            retain_accelerations=False,
-            linear_damping=0.0,
-            angular_damping=0.0,
-            max_linear_velocity=1000.0,
-            max_angular_velocity=1000.0,
-            max_depenetration_velocity=1.0,
-        ),
-        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=False, solver_position_iteration_count=4, solver_velocity_iteration_count=0
-        ),
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(0.0, 0.0, 0.55),
-        joint_pos={
-            ".*L_hip_joint": 0.1,
-            ".*R_hip_joint": -0.1,
-            "F[L,R]_thigh_joint": 0.8,
-            "R[L,R]_thigh_joint": 1.0,
-            "F[L,R]_calf_joint": -1.5,
-            "R[L,R]_calf_joint": -1.5,
-        },
-        joint_vel={".*": 0.0},
-    ),
-    soft_joint_pos_limit_factor=0.9,
-    actuators={
-        "base_legs": ALIENGO_ACTUATOR_IMPLICIT_CFG,
-    },
+UNITREE_ALIENGO_IMPLICIT_CFG = UNITREE_ALIENGO_DCMOTOR_CFG.replace(
+    actuators={"base_legs": ALIENGO_ACTUATOR_IMPLICIT_CFG}
 )
 """Configuration of Unitree AlienGo using Implicit actuator model."""
