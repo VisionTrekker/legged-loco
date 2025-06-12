33d5780c78d225f8fa7b5c4f08b04113b5845aaf
liuzhi <18082258131@163.com>
2025-06-12 08:50:12 +0800

    enlarge the low friction in rough_env_cfg to improve gait

--- source/leggedloco_tasks/leggedloco_tasks/manager_based/locomotion/config/aliengo/aliengo_rough_env_cfg.py
+++ source/leggedloco_tasks/leggedloco_tasks/manager_based/locomotion/config/aliengo/aliengo_rough_env_cfg.py
@@ -222,8 +222,8 @@ class EventCfg:
         mode="startup",
         params={
             "asset_cfg": SceneEntityCfg("robot", body_names=".*"),
-            "static_friction_range": (0.2, 0.8),
-            "dynamic_friction_range": (0.2, 0.6),
+            "static_friction_range": (0.4, 0.8),
+            "dynamic_friction_range": (0.4, 0.6),
             "restitution_range": (0.0, 0.0),
             "num_buckets": 64,
         },
