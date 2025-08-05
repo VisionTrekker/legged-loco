c38b86e066359b7e259b133b78c83cbb1d07ccbe
liuzhi <18082258131@163.com>
2025-08-05 13:41:50 +0800

    move quat2eulers to rsl_rl_utils.py

--- scripts/rsl_rl/rsl_rl_utils.py
+++ scripts/rsl_rl/rsl_rl_utils.py
@@ -1,4 +1,5 @@
 import torch
+import math
 
 import isaaclab.utils.math as math_utils
 
@@ -8,7 +9,7 @@ def camera_follow(env):
         camera_follow.smooth_camera_positions = []
     robot_pos = env.unwrapped.scene["robot"].data.root_pos_w[0]
     robot_quat = env.unwrapped.scene["robot"].data.root_quat_w[0]
-    camera_offset = torch.tensor([-5.0, 0.0, 1.0], dtype=torch.float32, device=env.device)
+    camera_offset = torch.tensor([-3.0, -3.0, 2.0], dtype=torch.float32, device=env.device)
     camera_pos = math_utils.transform_points(
         camera_offset.unsqueeze(0), pos=robot_pos.unsqueeze(0), quat=robot_quat.unsqueeze(0)
     ).squeeze(0)
@@ -22,3 +23,24 @@ def camera_follow(env):
     env.unwrapped.viewport_camera_controller.update_view_location(
         eye=smooth_camera_pos.cpu().numpy(), lookat=robot_pos.cpu().numpy()
     )
+
+
+def quat2eulers(q0, q1, q2, q3):
+    """
+    Calculates the roll, pitch, and yaw angles from a quaternion.
+
+    Args:
+        q0: The scalar component of the quaternion.
+        q1: The x-component of the quaternion.
+        q2: The y-component of the quaternion.
+        q3: The z-component of the quaternion.
+
+    Returns:
+        A tuple containing the roll, pitch, and yaw angles in radians.
+    """
+
+    roll = math.atan2(2 * (q2 * q3 + q0 * q1), q0 ** 2 - q1 ** 2 - q2 ** 2 + q3 ** 2)
+    pitch = math.asin(2 * (q1 * q3 - q0 * q2))
+    yaw = math.atan2(2 * (q1 * q2 + q0 * q3), q0 ** 2 + q1 ** 2 - q2 ** 2 - q3 ** 2)
+
+    return roll, pitch, yaw
